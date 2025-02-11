b62944acb7f2802b4afc5275c7cbd0249763ace9
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-02-11 10:33:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const student = {
+    name: "John Doe",
+};
+
+function getKeys(obj) {
+    return Object.keys(obj);
+}
+
+document.getElementById("get-keys-button").addEventListener("click", function () {
+    const keys = getKeys(student);
+    document.getElementById("result").innerText = `Keys: ${keys}`;
+});
